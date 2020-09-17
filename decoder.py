fb20ee7cfae649f78c70c6b458b78cb480fa9550
Yoav <norbit8@gmail.com>
2020-09-17 13:05:09 +0300

    Add files via upload

---
+++ decoder.py
@@ -0,0 +1,10 @@
+class decoder(object):
+    """
+    Decoder Class
+    """
+    # reduce the wastage of space and speed up the program by allocating space for a fixed amount of attributes
+    __slots__ = ['_input_dir', '_output_dir']
+
+    def __init__(self, input_dir: str, output_dir: str):
+        self._input_dir = input_dir
+        self._output_dir = output_dir
